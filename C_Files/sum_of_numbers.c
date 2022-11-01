4dd4c26cf0429db41a6ac02ce99f9ff907f9ba9e
Anshuman Raghuwanshi <ar8839396153@gmail.com>
2022-11-01 18:59:47 +0530

    Feat: Basic C program for sum of numbers

---
+++ C_Files/sum_of_numbers.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{   int n1,n2,n3, add;
+    printf("enter three number\n");
+    scanf("%d %d %d", &n1, &n2, &n3);
+    add = n1+n2+n3;
+    
+    printf("the sum of %d + %d +%d = %d" ,n1,n2,n3,add);
+
+    return 0;
+}
+
