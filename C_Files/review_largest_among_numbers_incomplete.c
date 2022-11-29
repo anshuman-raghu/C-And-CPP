b904103b8725678e1ed60ca47a0cc5671265fe5c
Anshuman Raghuwanshi <ar8839396153@gmail.com>
2022-11-29 17:25:46 +0530

    Refactor: Incomplete logic for finding largest number (for review)

---
+++ C_Files/review_largest_among_numbers_incomplete.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int
+main ()
+{
+  int a, b, c,d, l;
+  scanf ("%d%d%d%d", &a, &b, &c, %d);
+  
+  if (a>b)
+  {
+      if (a>c)
+      {
+          if(a>d)
+          {
+              if
+          }
+          
+      }
+      
+      
+  }
+  
+  
+  
+
+  printf ("the largest number is %d", l);
+  return 0;
+}
