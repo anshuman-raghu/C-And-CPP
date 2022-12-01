6d0785150a46e5e38af6dd24b6247ec6aad222af
Anshuman Raghuwanshi <ar8839396153@gmail.com>
2022-12-01 18:45:29 +0530

    Fix: Potential scanf issue in sum of digits (for review)

---
+++ C_Files/review_sum_of_digits_scanf_issue.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    char b;
+    printf("enter the three digit number\n");
+    scanf("%c",&a);
+    
+    b=a;
+    printf("\n%c",b);
+    /*if (b!=3)
+    
+    printf("\nyou did not enter a three digit number pls retry");*/
+    return 0;
+}
